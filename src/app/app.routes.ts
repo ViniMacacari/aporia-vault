46251fdc0a338f48c823ec6e102e0f1e64242f7a
ViniMacacari <macacarivinicius@gmail.com.br>
2025-03-25 13:41:01 -0300

    feat: vaults style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router'
 
 import { HomeComponent } from './pages/home/home.component'
+import { VaultComponent } from './pages/vault/vault.component'
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'vault', component: VaultComponent }
 ]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  vaults: any
 }
