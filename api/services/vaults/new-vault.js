4b59f8e0e4a08c9abf180aee69f4e08de4b2c9ea
ViniMacacari <macacarivinicius@gmail.com.br>
2025-03-27 18:00:54 -0300

    feat: implement pre-encryption and random string generation in NewVaultService; enhance ReadVaultService with seed generation for plausible deniability

--- api/services/vaults/new-vault.js
+++ api/services/vaults/new-vault.js
@@ -9,12 +9,28 @@ export class NewVaultService {
     deadContent = new DeadContent()
 
     async new(data) {
-        const content = await this.boost(data.content)
+        let content = ''
+
+        if (data.settings.aporiaKey === true) {
+            console.log('aqui2')
+            content = await this.preEncrytption(data.content)
+            content = JSON.stringify(content)
+            content = await this.boost(content)
+            console.log(content)
+        } else {
+            console.log('teste')
+            content = await this.boost(content)
+            console.log(content)
+        }
+
+        console.log(data.privateKey)
 
         let aporiaVault = await this.bepcrypt.encrypt({
             privateKey: data.privateKey,
             content: content
         })
+
+        console.log(aporiaVault)
 
         const safeFileName = data.fileName.replace(/\s+/g, '-')
         const filename = `${safeFileName}.aporia`
@@ -26,7 +42,7 @@ export class NewVaultService {
 
         const filePath = path.join(vaultsDir, filename)
 
-        if (data.settings.fakeWallet) {
+        if (data.settings.fakeWallet === true) {
             aporiaVault = 'plausibleDeniability===' + aporiaVault
         }
 
@@ -40,5 +56,35 @@ export class NewVaultService {
         const buffer = Buffer.from(raw, 'utf-8')
 
         return buffer.toString('base64')
+    }
+
+    async preEncrytption(content) {
+        const randomKey = await this.generateRandomString(48)
+        console.log(randomKey)
+        console.log(content)
+
+        let aporiaPreEncryption = await this.bepcrypt.encrypt({
+            privateKey: content,
+            content: randomKey
+        })
+
+        aporiaPreEncryption = Buffer.from(aporiaPreEncryption, 'utf-8')
+
+        return {
+            content: aporiaPreEncryption.toString('base64'),
+            key: randomKey
+        }
+    }
+
+    async generateRandomString(length) {
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%'
+        let result = ''
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * chars.length)
+            result += chars[randomIndex]
+        }
+
+        return result
     }
 }
--- api/services/vaults/read-vault.js
+++ api/services/vaults/read-vault.js
@@ -1,5 +1,6 @@
 import { BepCrypt } from '../../libs/bepcrypt/index.js'
 import { DeadContent } from '../security-boost/dead-content.js'
+import { GeneratePrivateKey } from '../../utils/generate-private-key.js'
 import path from 'path'
 import fs from 'fs/promises'
 import process from 'process'
@@ -7,6 +8,7 @@ import process from 'process'
 export class ReadVaultService {
     bepcrypt = new BepCrypt()
     deadContent = new DeadContent()
+    generator = new GeneratePrivateKey()
 
     async read(data) {
         const safeFileName = data.fileName.replace(/\s+/g, '-') + '.aporia'
@@ -23,14 +25,36 @@ export class ReadVaultService {
 
         const resetSettings = content.replace('plausibleDeniability===', '')
 
+        console.log(resetSettings)
+
         const decrypted = await this.bepcrypt.decrypt({
             content: resetSettings,
             privateKey: data.privateKey
         })
 
-        if (decrypted === null || decrypted === '') {
-            
+        console.log(decrypted)
+
+        if ((decrypted === null || decrypted === '') && hasPlausibleDeniability) {
+            const seed = this.generator.generateSeed()
+            const walletInfo = this.generator.getPrivateKeysFromMnemonic(seed)
+
+            return {
+                type: 'BIP-39',
+                vault: {
+                    generationDate: this.generateDate(),
+                    gapLimit: 20,
+                    aporiaKey: false,
+                    balance: {
+                        type: 'manual',
+                        lastBalance: +(Math.random() * 100).toFixed(8)
+                    }
+                },
+                seed: seed,
+                wallet: walletInfo
+            }
         }
+
+        if (decrypted.length === 0) throw 'Invalid key'
 
         const decoded = Buffer.from(decrypted, 'base64').toString('utf-8')
         const original = this.deadContent.extractContent(decoded)
@@ -45,5 +69,15 @@ export class ReadVaultService {
         } catch {
             return false
         }
+    }
+
+    generateDate() {
+        const now = Date.now()
+        const start = new Date('2025-01-01').getTime()
+        const end = Math.min(new Date('2025-12-31').getTime(), now)
+
+        const timestamp = Math.floor(Math.random() * (end - start)) + start
+        const date = new Date(timestamp)
+        return date.toISOString().split('T')[0]
     }
 }
