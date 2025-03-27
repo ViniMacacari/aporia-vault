06c48f6aff8e9a303eac47e256617bbaf56070a6
ViniMacacari <macacarivinicius@gmail.com.br>
2025-03-27 08:26:04 -0300

    feat: update dialog new vault component and styles

--- src/app/components/dialog-new-vault/dialog-new-vault.component.ts
+++ src/app/components/dialog-new-vault/dialog-new-vault.component.ts
@@ -60,6 +60,7 @@ export class DialogNewVaultComponent {
   digitalKey: boolean = true
 
   securePassword: string = ''
+  confirmSecurePassword: string = ''
 
   constructor(
     private keyType: KeyTypeDetectorService
