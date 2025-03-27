094eb68ee6c555268e22fbf89f8cfbc3061dd62d
ViniMacacari <macacarivinicius@gmail.com.br>
2025-03-27 09:49:19 -0300

    refactor: remove DeadContent class and related functionality from NewVaultService

--- api/services/vaults/new-vault.js
+++ api/services/vaults/new-vault.js
@@ -1,36 +1,28 @@
 import { BepCrypt } from '../../libs/bepcrypt/index.js'
-import { DeadContent } from '../security-boost/dead-content.js'
 import path from 'path'
 import fs from 'fs/promises'
 import process from 'process'
 
 export class NewVaultService {
     bepcrypt = new BepCrypt()
-    deadContent = new DeadContent()
 
     async new(data) {
-        const content = this.boostSecurity(data.content)
+        const aporiaVault = await this.bepcrypt.encrypt({
+            privateKey: data.privateKey,
+            content: data.content
+        })
 
-        // const aporiaVault = await this.bepcrypt.encrypt({
-        //     privateKey: data.privateKey,
-        //     content: content
-        // })
+        const safeFileName = data.fileName.replace(/\s+/g, '-')
+        const filename = `${safeFileName}.aporia`
 
-        // const safeFileName = data.fileName.replace(/\s+/g, '-')
-        // const filename = `${safeFileName}.aporia`
+        const baseDir = path.dirname(process.execPath)
+        const vaultsDir = path.join(baseDir, 'resources', 'vaults')
 
-        // const baseDir = path.dirname(process.execPath)
-        // const vaultsDir = path.join(baseDir, 'resources', 'vaults')
+        await fs.mkdir(vaultsDir, { recursive: true })
 
-        // await fs.mkdir(vaultsDir, { recursive: true })
+        const filePath = path.join(vaultsDir, filename)
+        await fs.writeFile(filePath, JSON.stringify(aporiaVault))
 
-        // const filePath = path.join(vaultsDir, filename)
-        // await fs.writeFile(filePath, JSON.stringify(aporiaVault))
-
-        // return content
-    }
-
-    async boostSecurity(content) {
-        this.deadContent.addContent(content)
+        return filePath
     }
 }
