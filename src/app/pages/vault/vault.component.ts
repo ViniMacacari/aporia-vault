f964005e085ca2b0eee2ddbc9c23124fdb374a3a
ViniMacacari <macacarivinicius@gmail.com>
2025-04-14 00:42:14 -0300

    feat: aporia vaults info

--- src/app/pages/vault/vault.component.ts
+++ src/app/pages/vault/vault.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { ActiveVaultsService } from '../../services/vaults/active-vaults.service'
+import { RouterTransitionService } from '../../services/transition/alter-transition.service'
 
 @Component({
   selector: 'app-vault',
@@ -8,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './vault.component.scss'
 })
 export class VaultComponent {
+  constructor(
+    private activeVaultsService: ActiveVaultsService,
+    private router: RouterTransitionService
+  ) { }
 
+  vault: any = {}
+
+  ngOnInit() {
+    this.vault = this.activeVaultsService.getVaultInfo()
+  }
+
+  return() {
+    this.router.navigate('/')
+  }
 }
--- src/app/services/vaults/active-vaults.service.ts
+++ src/app/services/vaults/active-vaults.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActiveVaultsService {
+  private storageKey = 'activeVault'
 
-  constructor() { }
+  constructor() {
+    const stored = sessionStorage.getItem(this.storageKey)
+    if (stored) {
+      this.vaultInfo = JSON.parse(stored)
+    }
+  }
 
-  vaultInfo: any = {}
+  private vaultInfo: any = {}
 
   setVaultInfo(vaultInfo: any): void {
     this.vaultInfo = vaultInfo
+    sessionStorage.setItem(this.storageKey, JSON.stringify(vaultInfo))
   }
 
   getVaultInfo(): any {
     return this.vaultInfo
+  }
+
+  clearVaultInfo(): void {
+    this.vaultInfo = {}
+    sessionStorage.removeItem(this.storageKey)
   }
 }
